de78fd09f02a11c6117e4a93754e62cc536889bf
Ahmed Hamza <81289670+ham-zax@users.noreply.github.com>
2021-12-25 21:40:46 +0530

    Added Hero section concept

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 const modules = [
   MatToolbarModule,
-  MatCardModule
+  MatCardModule,
+  MatGridListModule
 ]
   
 @NgModule({
